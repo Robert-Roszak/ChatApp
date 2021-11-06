ae5c559e85dfc681517e0bf94671df7c1fba0df0
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-06 17:25:29 +0100

    Add working server

--- client/app.js
+++ client/app.js
@@ -6,6 +6,12 @@ const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
 let userName;
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
+/* io.on('connection', (socket) => {
+    socket.on('message', ({ author, content }) => addMessage(author, content));
+}); */
 
 const login = (event) => {
     event.preventDefault();
@@ -19,16 +25,17 @@ const login = (event) => {
 
 const sendMessage = (event) => {
     event.preventDefault();
+    let messageContent = messageContentInput.value;
+
     if (messageContentInput.value) {
-        addMessage(userName, messageContentInput.value);
+        addMessage(userName, messageContent);
+        socket.emit('message', { author: userName, content: messageContent })
         messageContentInput.value = '';
     }
     else alert('You did not write anything');
 }
 
 const addMessage = (author, content) => {
-    console.log('author: ', author);
-    console.log('message: ', content);
     const message = document.createElement('li');
     message.classList.add('message');
     message.classList.add('message--received');
@@ -42,17 +49,6 @@ const addMessage = (author, content) => {
     `;
 
     messagesList.appendChild(message);
-
-    console.log(message);
-    /*
-Na starcie stwórz nowy element li i dodaj go do stałej message.
-Następnie dodaj do niego klasę .message oraz .message--received, która zadba o eleganckie wyłonienie się elementu.
-Jeśli author jest równe zmiennej globalnej userName, to dodaj do li również klasę .message--self. Będzie ona rozróżniała nasze wiadomości od tych wysłanych przez inne osoby.
-Następnie dodaj jako jego treść dwa kolejne elementy – nagłówek h3 o klasie message__author i diva o klasie message__content. Skąd wiemy, że akurat tak ma wyglądać struktura li? Po prostu w naszym pliku index.html mamy już przykładowe wiadomości, które wskazują, jak powinna być zbudowana ta struktura.
-Jako treść nagłówka wpisz wartość author lub You (jeśli author jest równe userName).
-Jako treść diva wpisz zawartość content.
-Na końcu dodaj element message do #messagesList.
-    */
 }
 
 loginForm.addEventListener('submit', login);
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
+
 const port = 8000;
 const messages = [];
 
 const app = express();
+
 
 app.use(express.static(path.join(__dirname, '/client/')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`)
-})
+});
+
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+  console.log('I\'ve added a listener on message event \n');
+});
